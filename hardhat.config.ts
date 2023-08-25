02b42c25f873351f3c43bb9d2dea25c494ef38cb
jiangplus <jiang.plus.times@gmail.com>
2023-08-25 17:15:05 +0800

    create project

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+  }
+};
+
+export default config;
