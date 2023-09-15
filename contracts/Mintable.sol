3097dd4145b74e5bb2e1ea827648bc3f421a0f56
jiangplus <jiang.plus.times@gmail.com>
2023-09-15 23:08:07 +0800

    adopt Ownable by Mintable

--- contracts/Mintable.sol
+++ contracts/Mintable.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Mintable is ERC721, AccessControl {
+contract Mintable is ERC721, Ownable {
     using Strings for uint256;
 
     string public baseURI;
@@ -22,34 +21,26 @@ contract Mintable is ERC721, AccessControl {
 
     constructor(string memory name, string memory symbol, string memory _baseURI) ERC721(name, symbol) {
         baseURI = _baseURI;
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(MINTER_ROLE, msg.sender);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
         return
             super.supportsInterface(interfaceId);
     }
 
-    function mint(address to) external {
-        require(hasRole(MINTER_ROLE, msg.sender), "caller is not a minter");
-
+    function mint(address to) external onlyOwner {
         _value += 1;
         _safeMint(to, _value);
     }
 
-    function mintMany(address to, uint256 amount) external {
-        require(hasRole(MINTER_ROLE, msg.sender), "caller is not a minter");
-
+    function mintMany(address to, uint256 amount) external onlyOwner {
         for (uint256 i = 0; i < amount; i+=1) {
             _value += 1;
             _safeMint(to, _value);
         }
     }
 
-    function mintBatch(address[] calldata addrs) external {
-        require(hasRole(MINTER_ROLE, msg.sender), "caller is not a minter");
-
+    function mintBatch(address[] calldata addrs) external onlyOwner {
         for (uint256 i = 0; i < addrs.length; i+=1) {
             _value += 1;
             _safeMint(addrs[i], _value);
@@ -66,9 +57,7 @@ contract Mintable is ERC721, AccessControl {
         return bytes(baseURI).length != 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : '';
     }
 
-    function setTokenURI(string memory _baseURI) external {
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "caller is not a minter");
-
+    function setTokenURI(string memory _baseURI) external onlyOwner {
         baseURI = _baseURI;
         emit BaseURIChanged(_baseURI);
     }
